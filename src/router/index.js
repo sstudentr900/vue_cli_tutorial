0a5cee2a45f624ddd001a607e7890de7a79600c3
John Doe <johndoe@example.com>
2022-03-24 23:05:40 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -75,11 +75,25 @@ const routes = [{
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-    // scrollBehavior() {
-    //     return { x: 0, y: 0, behavior: "smooth" };
-    // },
+        history: createWebHistory(process.env.BASE_URL),
+        routes,
+        // scrollBehavior() {
+        //     return { x: 0, y: 0, behavior: "smooth" };
+        // },
+    })
+    // 全域設置進入路由之前的時間點
+router.beforeEach((to, from, next) => {
+    // 當路由物件的 meta 設有 requiresAuth 時
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // 尚未登入
+        // if (!Token) {
+        //     next({ path: '/login' }) // 導向登入頁面
+        // } else {
+        //     next() // 登入成功，則可繼續往下執行
+        // }
+        next() // 登入成功，則可繼續往下執行
+    } else {
+        next() // 確保一定要使用 next()
+    }
 })
-
 export default router
--- src/views/storeView.vue
+++ src/views/storeView.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <h2>store</h2>
+        <h2>Vuex</h2>
+        <img src="https://vuex.vuejs.org/vuex.png" alt="">
+        <p>主要以四個核心概念——State、Getters、Mutations、Actions——所組成， Modules 可再進行模組化</p>
+        <p>dispatch -> action 發送 API 從後端取得資料後，commit -> mutation</p>
+        <p>mutation 改變 state，並且是唯一可以改變 state 的方法</p>
+        <p>透過 getter 取得 state 資料，再到元件中引入 getter 並將資料渲染至畫面上</p>
         <p>{{books}}</p>
     </div>
 </template>
