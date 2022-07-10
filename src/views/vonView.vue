ad7c89b44bee3acb2ef1e70bc3d802caf869fb13
John Doe <johndoe@example.com>
2022-07-10 15:27:58 +0800

    update

--- src/views/vonView.vue
+++ src/views/vonView.vue
@@ -1,20 +1,27 @@
 <template>
     <div>
         <h2>「@」v-on：監聽事件</h2>
-        <p>stop 阻擋事件冒泡</p>
+        <h4>事件修飾符</h4>
         <p>capture 以捕獲形式觸發事件</p>
-        <p>passive 是告訴瀏覽器，我沒使用 preventDefault 阻擋預設行為</p>
-        <p>self 只觸發元素自己的事件</p>
+        <p>stop  => stopPropagation 停止觸發上層 DOM 元素事件。</p>
+        <p>prevent => preventDefault 阻擋預設行為</p>
+        <p>self  => 只有觸發此 DOM 元素本身才會觸發 self 事件。</p>
+        <p>once  => 此事件只觸發一次。</p>
+        <p>passive => 無視 prevent 功能。</p>
+        <p>capture  => 會優先觸發。</p>
 
-        <h2>指定事件只觸發一次(@click.once)</h2>
+        <h4>指定事件只觸發一次(@click.once)</h4>
         <span>{{ clicks }}</span><br><br>
         <button type="button" @click="clicks += 1">Click it!</button><br><br>
         <button type="button" @click.once="clicks += 1">Click it!(只會被觸發一次)</button>
 
-        <h2>prevent 阻擋預設行為 event.preventDefault()</h2>
+        <h4>prevent 阻擋預設行為 event.preventDefault()</h4>
         <pre>
             form v-on:submit.prevent="onSubmit"
         </pre>
+
+        <hr>
+
 
         <h2>@input事件是实时监控的，每次输入都会调用</h2>
         <p>@keyup.enter需要点击回车键触发</p>
@@ -29,11 +36,16 @@
         <input type="text" :value="inputText"  @change="inputText = $event.target.value" />
         <p>顯示輸入內容：{{ inputText }}</p>
 
+        <hr>
+
+
         <h2>「:」v-bind：綁定 attribute</h2>
         <p>disabled: {{ isUnclickable }}</p>
         <button type="button" :disabled="isUnclickable" @click="disabled">
             Cannot click!
         </button>
+
+        <hr>
 
         <h2>綁定 class</h2>
         <p :class="{color_red : is_class}">class</p>
